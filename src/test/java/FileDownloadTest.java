999f2b1f19cb78d84c8fa5b2e3293a63c4249829
allaivanova1989 <baby20086@rambler.ru>
2021-12-07 16:37:15 +0300

    Add downloadFileTest

--- src/test/java/FileDownloadTest.java
+++ src/test/java/FileDownloadTest.java
@@ -1,3 +1,4 @@
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -6,41 +7,49 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 
+import static org.apache.commons.io.FileUtils.cleanDirectory;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class FileDownloadTest extends BaseTest {
     @Test
 
-    public void seleniumDownloadFile() throws InterruptedException {
+    public void seleniumDownloadFile() throws InterruptedException, IOException {
+        String pathToDownload = System.getProperty("user.dir") + "src\\test\\downloads";
+        String nameDownloadedFile = "sample.pdf";
+        int wait = 15000;
+
         HashMap<String, Object> chromePrefs = new HashMap<>();
         chromePrefs.put("profile.default_content_settings.popups", 0);
-        chromePrefs.put("download.default_directory", System.getProperty("user.dir"));
+        chromePrefs.put("download.default_directory", pathToDownload);
+
         ChromeOptions options = new ChromeOptions();
+
         options.setExperimentalOption("prefs", chromePrefs);
 
         WebDriver driver = new ChromeDriver(options);
         driver.navigate().to("http://the-internet.herokuapp.com/download");
-        WebElement ourFile = driver.findElement(By.xpath("//div[@class=\"example\"]/a[@href=\"download/test.png\"]"));
-        ourFile.click();
-        Thread.sleep(15000);
-        File folder = new File(System.getProperty("user.dir"));
+        WebElement ourFile = driver.findElement(By.xpath("//div[@class=\"example\"]/a[@href=\"download/sample.pdf\"]"));
+
+        FileUtils.cleanDirectory(new File(pathToDownload));
+        File folder;
+        folder = new File(pathToDownload);
         File[] listOfFiles = folder.listFiles();
-        boolean found = false;
-        File f = null;
-        for (File listOfFile : listOfFiles) {
-            if (listOfFile.isFile()) {
-                String fileName = listOfFile.getName();
-                System.out.println("File " + listOfFile.getName());
-                if (fileName.matches("test*.png")) {
-                    f = new File(fileName);
-                    found = true;
-                }
+        ourFile.click();
+        boolean fileIsNotReady = true;
+
+        while (wait != 0 && fileIsNotReady ) {
+            listOfFiles = folder.listFiles();
+            Thread.sleep(100);
+            wait -= 100;
+            if (listOfFiles.length != 0 && listOfFiles[0].getName().equals(nameDownloadedFile)) {
+                fileIsNotReady = false;
             }
         }
-        assertTrue(found, "Downloaded document is not found");
-        f.deleteOnExit();
+        assertEquals(listOfFiles[0].getName(), nameDownloadedFile, "Downloaded document is not found");
         driver.quit();
     }
 }
